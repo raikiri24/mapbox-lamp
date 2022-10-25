aa8f84bc15b635d6cf38ceff8b1b1867d2a972c4
Paul Leandro Lanot <paul.leanot.aesiph@gmail.com>
2022-10-25 19:35:29 +0800

    second version

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import MapComponent from './components/Map';
 import './App.css';
 import NavBar from './components/NavBar';
 
+
 function App() {
   return (
     <div className="App">
       <NavBar />
-
+      
       <MapComponent />
+      
     </div>
   );
 }
